d5a9b1e775a3daccbf20c7b4720ff96a80b9b328
AntoineRichard <antoine.richard@gatech.edu>
2021-09-02 11:26:46 +0000

    added support for HDF5 file sampling

--- samplers.py
+++ samplers.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import random
 import pickle
+import h5py
 import cv2
 
 class SimpleSampler:
@@ -96,3 +97,56 @@ class SequenceSampler:
                 images.append(raw_img)
                 positions.append(sample['data'][index]['center'])
         return np.array(positions)/512 - 0.5, np.array(images)/1.0, skip
+
+class CompressedPNGSequenceSampler:
+    def __init__(self, hdf5_path, pkl_path, seq_length=20, height=256, width=256):
+        # load args
+        self.height = height
+        self.width = width
+        self.seq_length = seq_length
+
+        # Load-Dataset
+        with open(pkl_path, 'rb') as handle:
+            self.dataset = pickle.load(handle)
+        self.h5 = h5py.File(hdf5_path)    
+        self.num_samples = len(self.dataset.keys())
+
+    def getDataset(self):
+        generator = self._generator
+        return tf.data.Dataset.from_generator(generator,
+                              args=[],
+                              output_types=(tf.float32, tf.float32),
+                              output_shapes = (tf.TensorShape([self.seq_length, self.height, self.width, 1]),tf.TensorShape([self.seq_length, 2])))
+
+    def _generator(self):
+        # Generator (to act as dataset)
+        keys = list(self.dataset.keys())
+        random.shuffle(keys)
+
+        for key in keys:
+            pose_seq, img_seq, skip = self._generateSequence(key)
+            if skip:
+                continue
+            else:
+                img_seq = np.expand_dims(img_seq,-1)
+            yield (img_seq, pose_seq)
+
+    def _generateSequence(self, key):
+        skip = False
+        positions = []
+        images = []
+        if self.dataset[key]['length'] < self.seq_length:
+            skip = True
+        else:
+            padding = self.dataset[key]['length'] - self.seq_length
+            offset = int(np.random.rand()*padding)
+            start = self.dataset[key]['start'] + offset
+            end = start + self.seq_length
+            for index in range(start, end, 1):
+                #print(self.h5[self.dataset[key]["tree_id"]][str(index-start)])
+                print(index)
+                raw_img = cv2.imdecode(self.h5[self.dataset[key]["tree_id"]][str(index)][:], cv2.IMREAD_UNCHANGED)
+                raw_img = cv2.resize(raw_img, (self.height, self.width))
+                images.append(raw_img)
+                positions.append(self.dataset[key]['centers'][index])
+        return np.array(positions)/512 - 0.5, np.array(images)/65535.0 - 0.5, skip

6ed55100316424bb3772467c31940d151a0526e3
anon556677 <antoine.richard@gatech.edu>
2021-09-09 13:29:49 +0200

    added support for more augmentation functions

--- utils.py
+++ utils.py
@@ -8,15 +8,17 @@ import scipy
 import cv2
 
 def histogramCut(img, shift = 0., cap = 1.):
-    """
-    Restricts the histogram to a rante between 'shift' and 'cap'.
+    """! Restricts the histogram to a range between 'shift' and 'cap'.
 
-    INPUT
-    img: an ND array [?, .., ?]
-    shift: a float between 0 and 1. With shift < cap.
-    cap: a float between 0 and 1. With cap > shift.
-    OUTPUT
-    img: an ND array [?, .., ?]
+    @type img: np.array
+    @param img: an ND array [?, .., ?]
+    @type shift: float
+    @param shift: a float between 0 and 1. With shift < cap.
+    @type cap: float
+    @param cap: a float between 0 and 1. With cap > shift.
+    
+    @rtype: np.array
+    @return: an ND array [?, .., ?]
     """
     img = img
     img[img>cap] = cap
@@ -28,27 +30,40 @@ def histogramCut(img, shift = 0., cap = 1.):
 
     return img
 
-def normalize(img):
-    """
-    Normalizes an image
+def normalizeTF(img):
+    """! Normalizes an image
 
-    INPUT
-    img: an ND array [?, .., ?]
-    OUTPUT
-    img: an ND array [?, .., ?]
+    @type img: tf.tensor
+    @param img: an ND tensor [?, .., ?]
+
+    @rtype: tf.tensor
+    @return: an ND tensor [?, .., ?]
+    """
+    img = img - tf.math.minimum(img)
+    img = img / tf.math.maximum(img)
+    return img
+
+def normalize(img):
+    """! Normalizes an image
+
+    @type img: np.array
+    @param img: an ND array [?, .., ?]
+
+    @rtype: np.array
+    @return: an ND array [?, .., ?]
     """
     img = img - np.amin(img)
     img = img / np.amax(img)
     return img
 
 def equalize(img):
-    """
-    Peforms adaptative histogram normalization on a flatten sequence of images
+    """! Peforms adaptative histogram normalization on a flatten sequence of images
 
-    INPUT
-    img: a 2D array [?, ?]
-    OUTPUT
-    ada: a 2D array [?, ?]
+    @type img: np.array
+    @param img: a 2D array [?, ?]
+    
+    @type ada: np.array
+    @param ada: a 2D array [?, ?]
     """
     ada = normalize(img)
     ada = ski.exposure.equalize_adapthist(ada, clip_limit=0.02)
@@ -59,56 +74,65 @@ def equalize(img):
     return ada
 
 def remove_image_block(x,y):
-    # 0 no mask, 1 mask from lower, 2 mask from higher
-    mask_height = int(np.random.rand()*3)
-    mask_width = int(np.random.rand()*3)
+    """! Randomly removes the lower part of an image or a sequence of images
+
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    @type y: tf.tensor
+    @param y: a tensor of shape [Sequence_size, 2]
+
+    @rtype: tuple
+    @return: a tuple of tensors of shape([Height, Width, Sequence_size], [Sequence_size, 2])
+    """
+    # 0 no mask, 1 mask from lower
+    mask_height = int(np.random.rand()*2)
+    #mask_width = int(np.random.rand()*3)
     min_pixels = 10
     max_pixels = 30
     x = np.array(x)
     if mask_height == 1:
         delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
         x[:,:int((y[0,0] + 0.5)*256 - delta)] = -0.5
-    elif mask_height == 2:
-        delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
-        x[:,int((y[0,0] + 0.5)*256 + delta):] = -0.5
-    if mask_width == 1:
-        delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
-        x[:int((y[0,1] + 0.5)*256 - delta),:] = -0.5
-    elif mask_width == 2:
-        delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
-        x[int((y[0,1] + 0.5)*256 + delta):,:] = -0.5
+    #elif mask_height == 2:
+    #    delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
+    #    x[:,int((y[0,0] + 0.5)*256 + delta):] = -0.5
+    #if mask_width == 1:
+    #    delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
+    #    x[:int((y[0,1] + 0.5)*256 - delta),:] = -0.5
+    #elif mask_width == 2:
+    #    delta = int(min_pixels + np.random.rand()*(max_pixels - min_pixels))
+    #    x[int((y[0,1] + 0.5)*256 + delta):,:] = -0.5
     return x, y
 
 def rotate(x, y):
-    """
-    Rotates a whole sequence by a random amount
+    """! Rotates a whole sequence by a random amount
 
-    INPUTS
-    x: a tensor of shape [Height, Width, Sequence_size]
-    y: a tensor of shape [Sequence_size, 2]
-    OUTPUTS:
-    x: a tensor of shape [Height, Width, Sequence_size]
-    y: a tensor of shape [Sequence_size, 2]
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    @type y: tf.tensor
+    @param y: a tensor of shape [Sequence_size, 2]
+    
+    @rtype: tuple
+    @return: a tuple of tensors of shape([Height, Width, Sequence_size], [Sequence_size, 2])
     """
     y = np.array(y)
     theta = np.random.rand()*2*np.pi
     c, s = np.cos(theta), np.sin(theta)
     R = np.array(((c, -s), (s, c)))
     y = np.matmul(R,y.T).T
-    # Rotate x
     x = scipy.ndimage.rotate(x, -180*theta/np.pi, reshape=False, cval=-0.5)
     return (x,y)
 
 def shift(x, y, max_shift=125):
-    """
-    Shifts a whole sequence by a random amount
+    """! Shifts a whole sequence by a random amount
 
-    INPUTS
-    x: a tensor of shape [Height, Width, Sequence_size]
-    y: a tensor of shape [Sequence_size, 2]
-    OUTPUTS:
-    x: a tensor of shape [Height, Width, Sequence_size]
-    y: a tensor of shape [Sequence_size, 2]
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    @type y: tf.tensor
+    @param y: a tensor of shape [Sequence_size, 2]
+    
+    @rtype: tuple
+    @return: a tuple of tensors of shape([Height, Width, Sequence_size], [Sequence_size, 2])
     """
     y = np.array(y)
     shifts = (np.random.rand(2) - 0.5)*max_shift/256
@@ -128,8 +152,8 @@ def augmentSequence(x,y):
     x: a tensor of shape [Sequence_size, Height, Width, 1]
     y: a tensor of shape [Sequence_size, 2]
     """
-    x = tf.squeeze(x)
-    x = tf.transpose(x,perm=[1,2,0])
+    x = tf.squeeze(x) # [S,H,W,1] -> [S,H,W]
+    x = tf.transpose(x,perm=[1,2,0]) # [S,H,W] -> [H,W,S]
     x,y = tf.py_function(rotate, [x,y], (tf.float32, tf.float32))
     x,y = tf.py_function(shift, [x,y], (tf.float32, tf.float32))
     x = tf.transpose(x,perm=[2,0,1])
@@ -148,9 +172,15 @@ def augmentImage(x,y):
     y: a tensor of shape [2]
     """
     y = tf.expand_dims(y,0)
-    x,y = tf.py_function(rotate, [x,y], (tf.float32, tf.float32))
-    x,y = tf.py_function(shift, [x,y], (tf.float32, tf.float32))
     x,y = tf.py_function(remove_image_block, [x,y], (tf.float32, tf.float32))
+    x,y = tf.py_function(rotate, [x,y], (tf.float32, tf.float32))
+    x,y = tf.py_function(remove_image_block, [x,y], (tf.float32, tf.float32))
+    x,y = tf.py_function(rotate, [x,y], (tf.float32, tf.float32))
+    x,y = tf.py_function(shift, [x,y], (tf.float32, tf.float32)) # replace by tf.roll ? More like a smart concat
+    x = randomBrightnessContrast(x)
+    x = gaussianNoise(x)
+    x = normalizeTF(x)
+    #x,y = tf.py_function(remove_image_block, [x,y], (tf.float32, tf.float32))
     #x = tf.expand_dims(x,-1)
     y = tf.squeeze(y)
     return x,y
@@ -184,24 +214,64 @@ def applyCorrectionImage(x):
     x = tf.expand_dims(x,-1)
     return x
 
-def randomAtenuation(x, max_attenuation=3):
+def randomBrightnessContrast(x, max_brightness_delta=0.2, max_contrast_factor=0.3):
+    """! Randomly changes images brightness and contrast.
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    
+    @rtype: tf.tensor
+    @return: a tensor of shape [Height, Width, Sequence_size]
     """
-    Selects one or more images of the sequence and randomly changes its brightness.
+    x = tf.image.random_brightness(x, max_brightness_delta)
+    x = tf.image.random_contrast(x, 1 - max_contrast_factor, 1 + max_contrast_factor)
+    return x
+
+def gaussianNoise(x, mean=0.0, max_sigma=0.005**0.5):
+    """! Adds a random amount of gausian noise of the images.
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    
+    @rtype: tf.tensor
+    @return: a tensor of shape [Height, Width, Sequence_size]
+    """
+    sigma = tf.random.uniform()*max_sigma
+    gauss = tf.random.normal(x.shape, mean, sigma)
+    x = x + gauss
+    return x
+
+def randomKill(x, max_kills=4, kill_ratio=0.2):
+    """! Selects one or more image of the sequence and randomly sets it to 0.
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+
+    @rtype: tf.tensor
+    @return: a tensor of shape [Height, Width, Sequence_size]
     """
     return x
 
-def randomKill(x, max_kills=1, kill_ratio=0.5):
+def randomBlock(x, max_block_size=32, max_boxes=3):
+    """! Creates blocks of random positions, values, and sizes in images.
+    @type x: tf.tensor
+    @param x: a tensor of shape [Height, Width, Sequence_size]
+    @type max_block_size: int
+    @param max_block_size: The maximum size of the blocks
+    @type max_boxes: int
+    @param max_boxes: The maximum number of boxes that can be added
+    
+    @rtype: tf.tensor
+    @return: a tensor of shape [Height, Width, Sequence_size]
     """
-    Selects one or more image of the sequence and randomly sets it to 0.
-    """
+    h, w, s = x.shape
+    for i in range(s):
+        boxes = int(np.random.rand()*max_boxes)
+        for _ in range(boxes):
+            xyhw = np.random.rand(4)
+            xy = int(xyhw[0:2]*256)
+            hw = int(xyhw[2:]*max_block_size)
+            x[xy[0]+hw[0]:xy[0]+hw[0],xy[1]+hw[1]:xy[1]+hw[1],i] = np.random.rand() - 0.5
     return x
 
-def randomBlock(x, max_kills=1, kill_ratio=0.5):
-    """
-    Adds blocks of random size and values at random positions in some images of the sequence
-    """
-    return x
-
+    
 
 def encodeGif(frames, fps):
     """
